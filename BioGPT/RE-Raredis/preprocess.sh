49ca36d052260e8b90fbd401c645b243a332fbbf
Shashank Gupta <shashankgupta140195@gmail.com>
2023-08-23 20:46:53 -0400

    bioGPT files

---
+++ BioGPT/RE-Raredis/preprocess.sh
@@ -0,0 +1,40 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+DATA_DIR=/content/BioGPT/data/Raredis
+prefix=relis
+RAW_DATA_DIR=${DATA_DIR}/raw
+OUTPUT_DIR=${DATA_DIR}/${prefix}-bin
+
+if [ -d "${OUTPUT_DIR}" ]; then
+    rm -rf ${OUTPUT_DIR}
+fi
+
+cp /content/BioGPT/data/dict.txt ${RAW_DATA_DIR}/
+cp /content/BioGPT/data/bpecodes ${RAW_DATA_DIR}/
+
+SPLIT=(train valid test)
+
+for ff in ${SPLIT[@]}; do
+    if [ -f "${RAW_DATA_DIR}/${prefix}_$ff.y" ]; then
+        echo "Preprocessing ${ff}"
+
+        perl /content/BioGPT/mosesdecoder/scripts/tokenizer/tokenizer.perl -l en -a -threads 8 < ${RAW_DATA_DIR}/${prefix}_$ff.x > ${RAW_DATA_DIR}/${prefix}_$ff.tok.x
+        perl /content/BioGPT/mosesdecoder/scripts/tokenizer/tokenizer.perl -l en -a -threads 8 < ${RAW_DATA_DIR}/${prefix}_$ff.y > ${RAW_DATA_DIR}/${prefix}_$ff.tok.y
+
+        /content/BioGPT/fastBPE/fast applybpe ${RAW_DATA_DIR}/${prefix}_$ff.tok.bpe.x ${RAW_DATA_DIR}/${prefix}_$ff.tok.x ${RAW_DATA_DIR}/bpecodes
+        /content/BioGPT/fastBPE/fast applybpe ${RAW_DATA_DIR}/${prefix}_$ff.tok.bpe.y ${RAW_DATA_DIR}/${prefix}_$ff.tok.y ${RAW_DATA_DIR}/bpecodes
+
+        rm ${RAW_DATA_DIR}/${prefix}_$ff.tok.x ${RAW_DATA_DIR}/${prefix}_$ff.tok.y
+    fi
+done
+
+# do binarize
+fairseq-preprocess \
+    -s x -t y --workers 8 \
+    --joined-dictionary \
+    --trainpref ${RAW_DATA_DIR}/${prefix}_train.tok.bpe \
+    --validpref ${RAW_DATA_DIR}/${prefix}_valid.tok.bpe \
+    --testpref ${RAW_DATA_DIR}/${prefix}_test.tok.bpe \
+    --destdir ${OUTPUT_DIR} \
+    --srcdict ${RAW_DATA_DIR}/dict.txt
