768d5d99d6ce72fb10523a2e5d499f37dd1e14eb
Shashank Gupta <shashankgupta140195@gmail.com>
2023-08-26 22:36:30 -0400

    Added Chemprot data

---
+++ Chemprot_verify/BioGPT/scripts/eval.py
@@ -0,0 +1,226 @@
+import json, os
+import pandas as pd
+
+true_positive_sum, pred_sum, true_sum = 0, 0, 0
+
+pred_file = "/Users/shashankgupta/Downloads/Chemprot/BioGPT/wo_ent_type/svo/generate_checkpoint_last.pt.detok.extracted.json"
+gold_file = "/Users/shashankgupta/Downloads/Chemprot/BioGPT/wo_ent_type/test.json"
+pmids_file = "/Users/shashankgupta/Downloads/Chemprot/BioGPT/wo_ent_type/svo/data/relis_test.pmid"
+
+
+def do_eval(preds, pmids, golden):
+    num_missing = 0
+    fp = 0
+    fn = 0
+    tn = 0
+
+    cpr3_fp, cpr3_tp, cpr3_fn = 0, 0, 0
+    cpr4_fp, cpr4_tp, cpr4_fn = 0, 0, 0
+    cpr5_fp, cpr5_tp, cpr5_fn = 0, 0, 0
+    cpr6_fp, cpr6_tp, cpr6_fn = 0, 0, 0
+    cpr9_fp, cpr9_tp, cpr9_fn = 0, 0, 0
+
+    columns = ['doc_name', 'gold_rels', 'pred_rels', 'rel_type', 'fp_prediction', "fn_prediction"]
+    df = pd.DataFrame(columns=columns)
+
+    for pred, idx in zip(preds, pmids):
+        print(idx)
+        gold_arg1_set, gold_arg2_set, gold_rel_set, gold_set = set(), set(), set(), set()
+        pred_arg1_set, pred_arg2_set, pred_rel_set, pred_set = set(), set(), set(), set()
+        gold_rel = ""
+        if idx not in golden:
+            num_missing += 1
+            # print("----Missing:", idx)
+            continue
+        if golden[idx]["triples"]:
+            for tp in golden[idx]["triples"]:
+                gold_rel = tp["rel"].strip().lower()
+                if gold_rel != "no relation":
+                    arg1 = tp["arg1"].strip().lower()
+                    arg2 = tp["arg2"].strip().lower()
+                    gold_arg1_set.add(arg1)
+                    gold_arg2_set.add(arg2)
+                    gold_rel_set.add(gold_rel)
+                    gold_set.add((arg1, arg2, gold_rel))
+
+        if pred["triple_list_pred"] and pred["triple_list_pred"][0]["subject"] != 'failed':
+            for tp in pred["triple_list_pred"]:
+                rel = tp["relation"].strip().lower()
+                if rel == "no relation" and gold_rel == "no relation":
+                    tn += 1
+                    continue
+
+                elif gold_rel == "no relation" and rel != "no relation":
+                    fp += len(pred_set)
+                    continue
+
+                elif gold_rel != "no relation" and rel == "no relation":
+                    fn += len(gold_set)
+                    continue
+
+                arg1 = tp["subject"].strip().lower()
+                arg2 = tp["object"].strip().lower()
+                pred_arg1_set.add(arg1)
+                pred_arg2_set.add(arg2)
+                pred_rel_set.add(rel)
+                pred_set.add((arg1, arg2, rel))
+
+        fp_rel = pred_rel_set - gold_rel_set
+
+        new_rows = [{'doc_name': idx, 'gold_rels': gold_rel_set, 'pred_rels': pred_rel_set, 'rel_type': "", 'fp_prediction': "", 'fn_prediction': ""}]
+        df = df.append(new_rows, ignore_index=True)
+
+        fp_dic = dict()
+        for x in fp_rel:
+            fp_lst = set()
+            for y in pred_set:
+                if y[2] == x:
+                    fp_lst.add(y)
+
+            if x == "cpr:3":
+                cpr3_fp += len(fp_lst)
+            elif x == "cpr:4":
+                cpr4_fp += len(fp_lst)
+            elif x == "cpr:5":
+                cpr5_fp += len(fp_lst)
+            elif x == "cpr:6":
+                cpr6_fp += len(fp_lst)
+            elif x == "cpr:9":
+                cpr9_fp += len(fp_lst)
+
+            fp_dic[x] = fp_lst
+
+        fn_dic = dict()
+        fp_dic_2 = dict()
+
+        for z in gold_rel_set:
+            gold = set()
+            prediction = set()
+            for f in gold_set:
+                if f[2] == z:
+                    gold.add(f)
+            for g in pred_set:
+                if g[2] == z:
+                    prediction.add(g)
+
+            fn_rel = gold - prediction
+            tp_rel = gold.intersection(prediction)
+            fp_rels = prediction - gold
+
+            if len(fn_rel) or len(tp_rel) or len(fp_rels)> 0:
+                if z == "cpr:3":
+                    cpr3_fn += len(fn_rel)
+                    cpr3_tp += len(tp_rel)
+                    cpr3_fp += len(fp_rels)
+                elif z == "cpr:4":
+                    cpr4_fn += len(fn_rel)
+                    cpr4_tp += len(tp_rel)
+                    cpr4_fp += len(fp_rels)
+                elif z == "cpr:5":
+                    cpr5_fn += len(fn_rel)
+                    cpr5_tp += len(tp_rel)
+                    cpr5_fp += len(fp_rels)
+                elif z == "cpr:6":
+                    cpr6_fn += len(fn_rel)
+                    cpr6_tp += len(tp_rel)
+                    cpr6_fp += len(fp_rels)
+                elif z == "cpr:9":
+                    cpr9_fn += len(fn_rel)
+                    cpr9_tp += len(tp_rel)
+                    cpr9_fp += len(fp_rels)
+
+                fn_dic[z] = fn_rel
+                fp_dic_2[z] = fp_rels
+
+        if len(fp_dic) > 0:
+            for a in fp_dic.keys():
+                new_rows = [{'doc_name': "", 'gold_rels': "", 'pred_rels': "", 'rel_type': a, 'fp_prediction': fp_dic[a], 'fn_prediction': ""}]
+                df = df.append(new_rows, ignore_index=True)
+
+        if len(fn_dic) > 0 or len(fp_dic_2) > 0:
+            for b in fn_dic.keys():
+                new_rows = [{'doc_name': "", 'gold_rels': "", 'pred_rels': "", 'rel_type': b, 'fp_prediction': fp_dic_2[b], 'fn_prediction': fn_dic[b]}]
+                df = df.append(new_rows, ignore_index=True)
+
+    # Produce Scores
+    P_cpr3 = cpr3_tp / (cpr3_tp + cpr3_fp)
+    R_cpr3 = cpr3_tp / (cpr3_tp + cpr3_fn)
+    Fscore_cpr3 = 2 * P_cpr3 * R_cpr3 / (P_cpr3 + R_cpr3)
+
+    print("cpr3 precision is: ", P_cpr3)
+    print("cpr3 Recall is: ", R_cpr3)
+    print("cpr3 F-score is: ", Fscore_cpr3)
+
+    # is_a Scores
+    P_cpr4 = cpr4_tp / (cpr4_tp + cpr4_fp)
+    R_cpr4 = cpr4_tp / (cpr4_tp + cpr4_fn)
+    Fscore_cpr4 = 2 * P_cpr4 * R_cpr4 / (P_cpr4 + R_cpr4)
+
+    print("cpr4 precision is: ", P_cpr4)
+    print("cpr4 Recall is: ", R_cpr4)
+    print("cpr4 F-score is: ", Fscore_cpr4)
+
+    # is_acron Scores
+    P_cpr9 = cpr9_tp / (cpr9_tp + cpr9_fp)
+    R_cpr9 = cpr9_tp / (cpr9_tp + cpr9_fn)
+    Fscore_cpr9 = 2 * P_cpr9 * R_cpr9 / (P_cpr9 + R_cpr9)
+
+    print("cpr9 precision is: ", P_cpr9)
+    print("cpr9 Recall is: ", R_cpr9)
+    print("cpr9 F-score is: ", Fscore_cpr9)
+
+    # is_synon Scores
+    P_cpr6 = cpr6_tp / (cpr6_tp + cpr6_fp)
+    R_cpr6 = cpr6_tp / (cpr6_tp + cpr6_fn)
+    Fscore_cpr6 = 2 * P_cpr6 * R_cpr6 / (P_cpr6 + R_cpr6)
+
+    print("cpr6 precision is: ", P_cpr6)
+    print("cpr6 Recall is: ", R_cpr6)
+    print("cpr6 F-score is: ", Fscore_cpr6)
+
+    # increase_risk_of Scores
+    P_cpr5 = cpr5_tp / (cpr5_tp + cpr5_fp)
+    R_cpr5 = cpr5_tp / (cpr5_tp + cpr5_fn)
+    Fscore_cpr5 = 2 * P_cpr5 * R_cpr5 / (P_cpr5 + R_cpr5)
+
+    print("cpr5 precision is: ", P_cpr5)
+    print("cpr5 Recall is: ", R_cpr5)
+    print("cpr5 F-score is: ", Fscore_cpr5)
+
+    # Overall
+    fp = cpr3_fp + cpr4_fp+ cpr5_fp + cpr6_fp + cpr9_fp
+    fn += cpr3_fn + cpr4_fn + cpr5_fn + cpr6_fn + cpr9_fn
+    tp = cpr3_tp + cpr4_tp + cpr5_tp + cpr6_tp + cpr9_tp
+
+    P_overall = tp / (tp+fp)
+    R_overall = tp / ( tp +fn)
+    F_overall = 2*P_overall*R_overall/ (P_overall + R_overall)
+
+    print("Overall precision is: ", P_overall)
+    print("Overall Recall is: ", R_overall)
+    print("Overall F-score is: ", F_overall)
+
+    #
+    # df.to_csv(
+    #     "/Users/shashankgupta/Downloads/Chemprot/BioGPT/with_ent_type/rel-is/error_analysis_rel_type_last.csv", index=False)
+
+
+preds = []
+with open(pred_file) as reader:
+    for line in reader:
+        preds.append(json.loads(line))
+
+with open(gold_file) as reader:
+    golden = json.load(reader)
+
+with open(pmids_file) as reader:
+    if '.json' in pmids_file:
+        pmids = json.load(reader)
+    else:
+        pmids = []
+        for line in reader:
+            pmids.append(line.strip())
+
+print("\n====File: ", os.path.basename(pred_file))
+do_eval(preds, pmids, golden)
+
---
+++ Chemprot_verify/BioGPT/scripts/rawToJSON_builder.py
@@ -0,0 +1,204 @@
+import json
+import pandas as pd
+from pandasql import sqldf
+
+train_abstracts = "/Users/Downloads/Chemprot/ChemProt_Corpus/chemprot_training" \
+                  "/chemprot_training_abstracts.tsv"
+dev_abstracts = "/Users/Downloads/Chemprot/ChemProt_Corpus/chemprot_development" \
+                "/chemprot_development_abstracts.tsv"
+test_abstracts = "/Users/Downloads/Chemprot/ChemProt_Corpus/chemprot_test_gs/chemprot_test_abstracts_gs" \
+                 ".tsv"
+
+train_entities = "/Users/Downloads/Chemprot/ChemProt_Corpus/chemprot_training/chemprot_training_entities.tsv"
+dev_entities = "/Users/Downloads/Chemprot/ChemProt_Corpus/chemprot_development/chemprot_development_entities.tsv"
+test_entities = "/Users/Downloads/Chemprot/ChemProt_Corpus/chemprot_test_gs/chemprot_test_entities_gs.tsv"
+
+train_relations = "/Users/Downloads/Chemprot/ChemProt_Corpus/chemprot_training/chemprot_training_relations.tsv"
+dev_relations = "/Users/Downloads/Chemprot/ChemProt_Corpus/chemprot_development/chemprot_development_relations.tsv"
+test_relations = "/Users/Downloads/Chemprot/ChemProt_Corpus/chemprot_test_gs/chemprot_test_relations_gs.tsv"
+
+xug_train_json = "/Users/shashankgupta/Downloads/Chemprot/files/train.json"
+xug_dev_json = "/Users/shashankgupta/Downloads/Chemprot/files/dev.json"
+xug_test_json = "/Users/shashankgupta/Downloads/Chemprot/files/test.json"
+
+
+def read(json_file, pred_file=None):
+    gold_docs = [json.loads(line) for line in open(json_file)]
+    if pred_file is None:
+        return gold_docs
+
+
+xug_train_file = read(json_file=xug_train_json)
+xug_dev_file = read(json_file=xug_dev_json)
+xug_test_file = read(json_file=xug_test_json)
+
+training_doc_keys = []
+dev_doc_keys = []
+
+for x in xug_train_file:
+    training_doc_keys.append(x["doc_key"])
+
+for y in xug_dev_file:
+    dev_doc_keys.append(y["doc_key"])
+
+column_names = ['doc_key', 'title', 'abstract']
+train_abs = pd.read_csv(train_abstracts, sep="\t", header=None)
+train_abs.columns = column_names
+dev_abs = pd.read_csv(dev_abstracts, sep="\t", header=None)
+dev_abs.columns = column_names
+combined_abs = train_abs.append(dev_abs)
+combined_abs = combined_abs.reset_index(drop=True)
+train_abs.drop(train_abs.index, inplace=True)
+dev_abs.drop(dev_abs.index, inplace=True)
+
+test_abs = pd.read_csv(test_abstracts, sep="\t", header=None)
+test_abs.columns = column_names
+
+column_names = ['doc_key', 'ent_id', 'ent_type', 'start', 'end', 'span']
+train_ents = pd.read_csv(train_entities, sep="\t", header=None)
+train_ents.columns = column_names
+dev_ents = pd.read_csv(dev_entities, sep="\t", header=None)
+dev_ents.columns = column_names
+combined_ents = train_ents.append(dev_ents)
+combined_ents = combined_ents.reset_index(drop=True)
+train_ents.drop(train_ents.index, inplace=True)
+dev_ents.drop(dev_ents.index, inplace=True)
+
+test_ents = pd.read_csv(test_entities, sep="\t", header=None)
+test_ents.columns = column_names
+
+column_names = ['doc_key', 'rel_type', 'misc', 'fine_grained_rel_type', 'arg1', 'arg2']
+train_rels = pd.read_csv(train_relations, sep="\t", header=None)
+train_rels.columns = column_names
+dev_rels = pd.read_csv(dev_relations, sep="\t", header=None)
+dev_rels.columns = column_names
+combined_rels = train_rels.append(dev_rels)
+combined_rels = combined_rels.reset_index(drop=True)
+train_rels.drop(train_rels.index, inplace=True)
+dev_rels.drop(dev_rels.index, inplace=True)
+
+test_rels = pd.read_csv(test_relations, sep="\t", header=None)
+test_rels.columns = column_names
+
+mismatch = 0
+unicode = ['\xa0', '\u2002', '\u2005', '\u2009', '\u200a']
+
+for z in training_doc_keys:
+    selected_abs = combined_abs[combined_abs['doc_key'] == int(z)]
+    selected_ents = combined_ents[combined_ents['doc_key'] == int(z)]
+    selected_rels = combined_rels[combined_rels['doc_key'] == int(z)]
+
+    train_abs = train_abs.append(selected_abs)
+    train_ents = train_ents.append(selected_ents)
+    train_rels = train_rels.append(selected_rels)
+
+train_abs = train_abs.reset_index(drop=True)
+train_ents = train_ents.reset_index(drop=True)
+train_rels = train_rels.reset_index(drop=True)
+
+for y in dev_doc_keys:
+    selected_abs_dev = combined_abs[combined_abs['doc_key'] == int(y)]
+    selected_ents_dev = combined_ents[combined_ents['doc_key'] == int(y)]
+    selected_rels_dev = combined_rels[combined_rels['doc_key'] == int(y)]
+
+    dev_abs = dev_abs.append(selected_abs_dev)
+    dev_ents = dev_ents.append(selected_ents_dev)
+    dev_rels = dev_rels.append(selected_rels_dev)
+
+dev_abs = dev_abs.reset_index(drop=True)
+dev_ents = dev_ents.reset_index(drop=True)
+dev_rels = dev_rels.reset_index(drop=True)
+
+
+rels_to_keep = ['CPR:3', 'CPR:4', 'CPR:5', 'CPR:6', 'CPR:9']
+
+
+# change these to create data
+# test
+table_abs = test_abs
+table_ent = test_ents
+table_res = test_rels
+
+# # train
+# table_abs = train_abs
+# table_ent = train_ents
+# table_res = train_rels
+
+# dev
+# table_abs = dev_abs
+# table_ent = dev_ents
+# table_res = dev_rels
+
+final_doc = dict()
+with open('/Users/shashankgupta/Downloads/Chemprot/BioGPT/wo_ent_type/test.json', 'w') as outfile:
+    for x in range(len(table_abs)):
+        doc = table_abs["doc_key"][x]
+        print(doc)
+        # if doc != 23214423:
+        #     continue
+        title = table_abs["title"][x]
+        abstract = table_abs["abstract"][x]
+        context = title + " " + abstract
+        context = context.lower()
+        for item in unicode:
+            context = context.replace(item, u' ')
+
+        query = "SELECT * FROM table_ent WHERE doc_key = '" + str(int(doc)) + "'"
+        result = sqldf(query)
+        result = result.fillna("NA")
+
+        ent_info_dict = dict()
+        for i in range(len(result)):
+            ent_id = result["ent_id"][i]
+            ent_type = result["ent_type"][i].lower()
+            if ent_type == "gene-n" or ent_type == "gene-y":
+                ent_type = "gene"
+            start = result["start"][i]
+            end = result["end"][i]
+            span = result["span"][i].lower()
+            context_span = context[start: end].lower()
+
+            if span != context_span:
+                print("mismatch in doc key ", doc, "anno span:", span, "context_span: ", context_span)
+                mismatch += 1
+
+            ent_info_dict[ent_id] = {"type": ent_type, "span": span}
+
+        selected_row = table_res[table_res['doc_key'] == doc]
+        selected_row = selected_row[selected_row['rel_type'].isin(rels_to_keep)]
+        selected_row = selected_row.reset_index(drop=True)
+
+        triples = []
+        output_string = ""
+        if len(selected_row) == 0:
+            triples.append({"rel": "no relation"})
+
+        else:
+            for k in range(len(selected_row)):
+                temp = dict()
+                arg1_id = (selected_row["arg1"][k])[5:]
+                arg2_id = (selected_row["arg2"][k])[5:]
+
+                # with ent_Type
+                # arg1 = ent_info_dict[arg1_id]["type"] + " " + ent_info_dict[arg1_id]["span"]
+                # arg2 = ent_info_dict[arg2_id]["type"] + " " + ent_info_dict[arg2_id]["span"]
+
+                # w/o ent_type
+                arg1 = ent_info_dict[arg1_id]["span"]
+                arg2 = ent_info_dict[arg2_id]["span"]
+
+                rel_type = (selected_row["rel_type"][k]).lower()
+
+                temp["rel"] = rel_type
+                temp["arg1"] = arg1
+                temp["arg2"] = arg2
+                triples.append(temp)
+
+        final_doc[int(doc)] = {"abstract": "consider the abstract: $ " + context + "$ from the given abstract, all the "
+                                                                              "entities and relations among them are: "}
+        final_doc[int(doc)]["triples"] = triples
+        final_doc[int(doc)]["pmid"] = int(doc)
+
+    json.dump(final_doc, outfile)
+
+
